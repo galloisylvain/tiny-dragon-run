1d0f588a4b3462ff42fa612eb95c9f17170ae864
Sylvain Gallois <galloisylvain@gmail.com>
2018-06-11 21:19:24 +0200

    game

---
+++ Tiny Dragon Run/Assets/Scripts/PlayerMoves.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoves : MonoBehaviour {
+
+    public float thrust;
+    public Rigidbody2D rb;
+    float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        speed = 10.0f;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            rb.velocity = Vector2.up * thrust;
+        }
+    }
+}
